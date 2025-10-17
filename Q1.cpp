8198bc34cca0ad79b28a67b29848a334c104fed6
mems250005025 <mems250005025@iiti.ac.in>
2025-10-17 16:40:57 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,31 @@
+//Write a C++ program to define a structure named ‘Item‘ having members ‘id‘ (integer) and ‘cost‘ (float), declare an array of 6 ‘Item‘ structures, initialize the first four elements with sample data, and then use a ‘for‘ loop to iterate through the entire array, displaying the details of any item whose ‘cost‘ is greater than 50.00.
+#include <iostream>
+using namespace std;
 
+struct Item
+{
+    int id;
+    float cost;
+};
+
+int main() 
+{
+    
+    Item items[6] = {
+        {1, 45.50},  
+        {2, 75.25},  
+        {3, 35.00},  
+        {4, 100.00}, 
+        {0, 0.00},   
+        {0, 80.00} };
+
+    cout << "Items with cost greater than 50.00:" << endl;
+    for (int i = 0; i < 6; i++) 
+    {
+        if (items[i].cost > 50.00) {
+            cout << "Item ID: " << items[i].id << ", Cost: " << items[i].cost << endl;
+        }
+    }
+
+    return 0;
+}
